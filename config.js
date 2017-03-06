f6e8058b4ce674205d244bdd6936f3b4c908bbf0
Alexander Close <email@alexanderclose.com>
2017-03-05 19:29:43 -0800

    Configure heroku postgres settings

--- config.js
+++ config.js
@@ -11,19 +11,24 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://alexanderclose-ghost.herokuapp.com',
+        fileStorage: false,
         mail: {},
         database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
+          client: 'postgres',
+          connection: {
+            host: process.env.POSTGRES_HOST,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+            port: '5432'
+          },
+          debug: false
         },
 
         server: {
-            host: '127.0.0.1',
-            port: '2368'
+            host: '0.0.0.0',
+            port: process.env.PORT
         }
     },
 
